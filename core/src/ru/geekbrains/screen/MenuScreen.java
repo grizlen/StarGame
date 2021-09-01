5395b97ef9c7b30e662c353e452b77a438d14857
grizlen <grizlen@rambler.ru>
2021-09-02 03:17:46 +0500

    lesson3

--- core/src/ru/geekbrains/screen/MenuScreen.java
+++ core/src/ru/geekbrains/screen/MenuScreen.java
@@ -1,67 +1,55 @@
 package ru.geekbrains.screen;
 
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
+import ru.geekbrains.math.Rect;
+import ru.geekbrains.sprite.Background;
 
 public class MenuScreen extends BaseScreen {
 
-    private static final float IMG_SPEED = 2f;
     private Texture img;
-    private final Vector2 pos = new Vector2();
-    private final Vector2 offs = new Vector2();
-    private final Vector2 touchPos = new Vector2();
-    private final Vector2 vel = new Vector2();
-    private final Vector2 imgPos = new Vector2();
+    private Texture bg;
+
+    private Background background;
 
     @Override
     public void show() {
         super.show();
         img = new Texture("badlogic.jpg");
-        offs.set(img.getWidth() / 2, img.getWidth() / 2);
-        touchPos.set(Gdx.graphics.getWidth() / 2, Gdx.graphics.getHeight() / 2);
-        pos.set(touchPos);
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
     }
 
 
     @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+    }
+
+    @Override
     public void render(float delta) {
         super.render(delta);
-        batch.begin();
-        UpdatePos();
-        imgPos.set(pos).sub(offs);
-        batch.draw(img, imgPos.x, imgPos.y);
-        batch.end();
+        update(delta);
+        draw();
     }
 
     @Override
     public void dispose() {
         super.dispose();
         img.dispose();
+        bg.dispose();
     }
 
-    @Override
-    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
-        float x = screenX < offs.x ? offs.x :
-                screenX + offs.x > Gdx.graphics.getWidth() ? Gdx.graphics.getWidth() - offs.x :
-                        screenX;
-        screenY = Gdx.graphics.getHeight() - screenY;
-        float y = screenY < offs.y ? offs.y :
-                screenY + offs.y > Gdx.graphics.getHeight() ? Gdx.graphics.getHeight() - offs.y :
-                        screenY;
-        touchPos.set(x, y);
-        UpdatePos();
-        return false;
+    private void update(float delta) {
+
     }
 
-    private void UpdatePos() {
-        vel.set(touchPos).sub(pos);
-        if (vel.len() > IMG_SPEED) {
-            pos.add(vel.nor().scl(IMG_SPEED));
-        } else {
-            pos.set(touchPos);
-        }
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        batch.draw(img, 0, 0, 0.3f, 0.3f);
+        batch.end();
     }
 }
