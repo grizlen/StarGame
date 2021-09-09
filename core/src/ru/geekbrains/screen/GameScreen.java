1ed52b1e782d21159201179d486dd3cbffec8576
grizlen <grizlen@rambler.ru>
2021-09-10 02:25:43 +0500

    w4_3

--- core/src/ru/geekbrains/screen/GameScreen.java
+++ core/src/ru/geekbrains/screen/GameScreen.java
@@ -1,12 +1,15 @@
 package ru.geekbrains.screen;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.base.BaseScreen;
 import ru.geekbrains.math.Rect;
 import ru.geekbrains.sprite.Background;
+import ru.geekbrains.sprite.Ship;
 import ru.geekbrains.sprite.Star;
 
 public class GameScreen extends BaseScreen {
@@ -15,17 +18,23 @@ public class GameScreen extends BaseScreen {
     private TextureAtlas atlas;
     private Background background;
     private Star[] stars;
+    private Ship ship;
 
     @Override
     public void show() {
         super.show();
 
         bg = new Texture("textures/bg.png");
-        atlas = new TextureAtlas("textures/menuAtlas.tpack");
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
 
         background = new Background(bg);
         stars = new Star[STAR_COUNT];
         for (int i = 0; i < stars.length; stars[i++] = new Star(atlas));
+        TextureAtlas.AtlasRegion rgn = atlas.findRegion("main_ship");
+        int tw = rgn.getRegionWidth() / 2;
+        int th = rgn.getRegionHeight();
+        TextureRegion[][] split = rgn.split(tw, th);
+        ship = new Ship(split[0][0]);
     }
 
     @Override
@@ -42,6 +51,7 @@ public class GameScreen extends BaseScreen {
         for (Star star: stars) {
             star.resize(worldBounds);
         }
+        ship.resize(worldBounds);
     }
 
     @Override
@@ -53,18 +63,31 @@ public class GameScreen extends BaseScreen {
 
     @Override
     public boolean touchDown(Vector2 touch, int pointer, int button) {
-        return super.touchDown(touch, pointer, button);
+        return ship.touchDown(touch, pointer, button);
     }
 
     @Override
     public boolean touchUp(Vector2 touch, int pointer, int button) {
-        return super.touchUp(touch, pointer, button);
+        return ship.touchUp(touch, pointer, button);
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        ship.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        ship.keyUp(keycode);
+        return false;
     }
 
     private void update(float delta) {
         for (Star star: stars) {
             star.update(delta);
         }
+        ship.update(delta);
     }
 
     private void draw() {
@@ -73,6 +96,7 @@ public class GameScreen extends BaseScreen {
         for (Star star: stars) {
             star.draw(batch);
         }
+        ship.draw(batch);
         batch.end();
     }
 }
